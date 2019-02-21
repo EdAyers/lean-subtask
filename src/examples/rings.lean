7c92e751fd44881071c929b1d7884c8c5928b548
E.W.Ayers <E.W.Ayers@maths.cam.ac.uk>
2019-02-21 11:52:03 +0000

    Rejig rings.

--- src/examples/rings.lean
+++ src/examples/rings.lean
@@ -1,15 +1,17 @@
-import ..equate
+import ..equate tactic
 open robot
 universe u 
 section rings
     -- [TODO] one of the examples in `group` is to show this, so there should be a way of telling `equate` to omit rules.
     attribute [equate] neg_sub 
 
-    variables {α : Type u} [comm_ring α] {a b c d e : α}
+    variables {α : Type u} [comm_ring α] {a b c d e x y z : α}
     example : (a + b) * (a + b) = a * a + 2 * (a * b) + b * b := 
     by equate
-    -- example : (a + b) * (a - b) = a * a - b * b := 
-    -- by equate
+    example : (a * -d - b * - c) * e = -((a * d - b * c) * e) := 
+    by equate
+    @[equate] lemma sub_minus : - (a - b) = - a - - b := by equate
+    -- comparison with new lemma added
     example : (a * -d - b * - c) * e = -((a * d - b * c) * e) := 
     by equate
     example : (a * d) * b + b * (c * e) = (a * d + c * e) * b := 
@@ -19,10 +21,22 @@ section rings
     example : (a + c) * b = a * b + b * c := 
     by equate
     
+    /- Failures: -/
+
+    example : (a + b) * (a - b) = a * a - b * b := 
+    by equate
     example : (a * b) - c + (b * a) = - c + 2 * (a *  b) := 
     by equate  
     example : (a * b) - c + (b * a) = - c + (2 * a) *  b := 
     by equate  
     example : (a * b) - c + (b * a) = - c + a * (b + b) := 
     by equate  
+
+    /- Comparison of proof lengths. -/
+    lemma e1 : (x+y)^2+(x+z)^2 = (z+x)^2+(y+x)^2 := by equate
+    #print e1
+    lemma e2 : (x+y)^2+(x+z)^2 = (z+x)^2+(y+x)^2 :=
+         by ring
+    #print e2
+
 end rings
